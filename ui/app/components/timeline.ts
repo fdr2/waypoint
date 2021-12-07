94b331a246d1de16dfd18382cf815e4f04086cd0
Sabrina Ko <sabrina@hashicorp.com>
2021-12-07 15:25:08 -0500

    ui: refining timeline component/remove extraneous translations

--- ui/app/components/timeline.ts
+++ ui/app/components/timeline.ts
@@ -1,11 +1,21 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import RouterService from '@ember/routing/router-service';
-import { DeploymentExtended, BuildExtended, ReleaseExtended } from 'waypoint/services/api';
 import ApiService from 'waypoint/services/api';
 import { Status } from 'waypoint-pb';
 
-interface Artifact {
+const _TYPE_TRANSLATIONS = {
+  build: 'page.artifact.timeline.build',
+  deployment: 'page.artifact.timeline.deployment',
+  release: 'page.artifact.timeline.release',
+};
+
+const _TYPE_ROUTES = {
+  build: 'workspace.projects.project.app.build',
+  deployment: 'workspace.projects.project.app.deployment.deployment-seq',
+  release: 'workspace.projects.project.app.release',
+};
+interface ArtifactModel {
   sequence: number;
   type: string;
   route: string;
@@ -23,28 +33,15 @@ interface TimelineArtifact {
   status: Status.AsObject | undefined;
 }
 interface TimelineArgs {
-  currentArtifact: DeploymentExtended | BuildExtended | ReleaseExtended;
-  timeline: TimelineModel;
+  model: TimelineModel;
 }
-
-let TYPE_TRANSLATIONS = {
-  build: 'page.artifact.timeline.build',
-  deployment: 'page.artifact.timeline.deployment',
-  release: 'page.artifact.timeline.release',
-};
-
-let TYPE_ROUTES = {
-  build: 'workspace.projects.project.app.build',
-  deployment: 'workspace.projects.project.app.deployment.deployment-seq',
-  release: 'workspace.projects.project.app.release',
-};
 
 export default class Timeline extends Component<TimelineArgs> {
   @service api!: ApiService;
   @service router!: RouterService;
 
   areWeHere(currentArtifactKey: string): boolean {
-    let entry = Object.entries(TYPE_ROUTES).find(([_, value]) =>
+    let entry = Object.entries(_TYPE_ROUTES).find(([_, value]) =>
       this.router.currentRouteName.includes(value)
     );
 
@@ -54,17 +51,16 @@ export default class Timeline extends Component<TimelineArgs> {
     return false;
   }
 
-  get artifacts(): Artifact[] {
-    let artifactsList: Artifact[] = [];
-    for (let key in this.args.timeline) {
-      let artifactObj = {
-        sequence: this.args.timeline[key].sequence,
-        type: TYPE_TRANSLATIONS[key],
-        route: TYPE_ROUTES[key],
-        status: this.args.timeline[key].status,
+  get artifacts(): ArtifactModel[] {
+    let artifactsList: ArtifactModel[] = [];
+    for (let key in this.args.model) {
+      artifactsList.push({
+        sequence: this.args.model[key].sequence,
+        type: _TYPE_TRANSLATIONS[key],
+        route: _TYPE_ROUTES[key],
+        status: this.args.model[key].status,
         isCurrentRoute: this.areWeHere(key),
-      } as Artifact;
-      artifactsList.push(artifactObj);
+      } as ArtifactModel);
     }
     return artifactsList;
   }
